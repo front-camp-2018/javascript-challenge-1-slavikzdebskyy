5f2914fc5fa790c176acfcd092c9dda14b704b34
slavikzdebskyy <zdebskyy.slavik@gmail.com>
2018-10-30 00:22:40 +0200

    Well done! All tests passed

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,14 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
-  // your logic here...
+  // your logic here... 
+  const str = num.toString();
+  if (str.length % 2) {    
+    const firstPart = str.slice(0, (str.length + 1) / 2);
+    const secondPart = str.slice((str.length + 1) / 2, str.length);
+    return firstPart === secondPart ? num : num * 2;
+  } else {
+    const firstPart = str.slice(0, str.length / 2);
+    const secondPart = str.slice(str.length / 2, str.length);
+    return firstPart === secondPart ? num : num * 2;
+  }  
 };
--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,4 +1,15 @@
 /* eslint-disable-next-line */
 export const sortByWeight = arr => {
   // your logic here...
+  const sum = array => {
+    const numbersArray = array.split('').map(el => {
+      return Number(el);
+    });
+    return numbersArray.reduce((accum, el) => {
+      return accum += el;
+    }, 0);
+  };
+  return arr.sort((a, b) => {
+    return sum(a) - sum(b);
+  });   
 };

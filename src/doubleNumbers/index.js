a77e843b92f7a9e44929b079e683b24e76ad978f
slavikzdebskyy <zdebskyy.slavik@gmail.com>
2018-11-04 01:21:28 +0200

    I fixed function doubleNum after Volodymyr's comments

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -2,13 +2,8 @@
 export const doubleNum = num => {
   // your logic here... 
   const str = num.toString();
-  if (str.length % 2) {    
-    const firstPart = str.slice(0, (str.length + 1) / 2);
-    const secondPart = str.slice((str.length + 1) / 2, str.length);
-    return firstPart === secondPart ? num : num * 2;
-  } else {
-    const firstPart = str.slice(0, str.length / 2);
-    const secondPart = str.slice(str.length / 2, str.length);
-    return firstPart === secondPart ? num : num * 2;
-  }  
+  const index = str.length % 2 ? (str.length + 1) / 2 : str.length / 2;     
+  const firstPart = str.slice(0, index);
+  const secondPart = str.slice(index, str.length);
+  return firstPart === secondPart ? num : num * 2; 
 };

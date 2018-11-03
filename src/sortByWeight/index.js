978c8d0acf173dfb459295e9db4972d4eed867d5
slavikzdebskyy <zdebskyy.slavik@gmail.com>
2018-11-04 01:27:50 +0200

    I fixed function sortByWeight after Volodymyr's comments

--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -2,11 +2,8 @@
 export const sortByWeight = arr => {
   // your logic here...
   const sum = array => {
-    const numbersArray = array.split('').map(el => {
-      return Number(el);
-    });
-    return numbersArray.reduce((accum, el) => {
-      return accum += el;
+    return array.split('').reduce((accum, el) => {
+      return accum += Number(el);
     }, 0);
   };
   return arr.sort((a, b) => {
